90857b9d4aa294b796681a10178fb408d36aeb6b
admin <ar.mshahs@gmail.com>
2024-07-16 22:01:14 +0530

    0.4

--- frontend/src/components/Home/RecentlyAdded.jsx
+++ frontend/src/components/Home/RecentlyAdded.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import BookCard from "../BookCard/BookCard";
 
 const RecentlyAdded = () => {
+  const [Data, setData] = useState();
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios
+        .get("http://localhost:1000/api/v1/get-recent-books")
+        .then()
+        .catch(error);
+      setData(response.data.data);
+    };
+    fetch();
+  }, []);
   return (
     <div className="mt-8 ">
       <h4 className="text-2xl text-yellow-100">Recently Added Books</h4>
+      <div className="my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
+        {Data &&
+          Data.map((items, i) => {
+            <div key={i}>
+              <BookCard data={items} />{" "}
+            </div>;
+          })}
+      </div>
     </div>
   );
 };

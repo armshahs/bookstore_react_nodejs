b2685ba4cfda6771b01c91b0dc3a4edde42c6a01
admin <ar.mshahs@gmail.com>
2024-07-16 09:43:59 +0530

    0.1

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="bg-zinc-800 text-white px-4 py-2">
+        <div>Item 1</div>
+        <div>Item 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

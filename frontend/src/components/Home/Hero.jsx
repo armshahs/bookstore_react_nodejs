60c78c7f34bb17f1778695d8b37cb352e98f4381
admin <ar.mshahs@gmail.com>
2024-07-16 19:32:15 +0530

    0.3

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -1,20 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] flex flex-col lg:flex-row">
-      <div className="w-full lg:w-3/6 flex flex-col items-center lg:items-start justify-center">
-        <h1 className="text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left">
+    <div className="h-[75vh] flex flex-col md:flex-row items-center justify-center">
+      <div className="w-full mb-12 md:mb-0 lg:w-3/6 flex flex-col items-center md:items-start justify-center">
+        <h1 className="text-4xl lg:text-6xl font-semibold text-yellow-100 text-center md:text-left">
           Discover Your Next Great Book
         </h1>
-        <p className="mt-4 text-xl text-zinc-300 text-center lg:text-left">
+        <p className="mt-4 text-xl text-zinc-300 text-center md:text-left">
           Explore captivating stories, enrich your mind and find boundless
           inspiration in our curated collection of books.
         </p>
         <div className="mt-8">
-          <button className="text-yellow-100 text-lg lg:text-xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full">
+          <Link
+            to="/all-books"
+            className="text-yellow-100 text-lg md:text-xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full"
+          >
             Discover Books
-          </button>
+          </Link>
         </div>
       </div>
       <div className=" w-full lg:w-3/6 h:auto lg:h-[100] flex items-center justify-center ">
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   const links = [
@@ -21,19 +22,24 @@ const Navbar = () => {
     },
   ];
 
+  const [MobileNav, setMobileNav] = useState("hidden");
+  const HandleMobileNav = () => {
+    MobileNav === "hidden" ? setMobileNav("block") : setMobileNav("hidden");
+  };
+
   return (
-    <div>
-      <div className="bg-zinc-800 text-white px-4 py-4 flex items-center justify-between">
-        <div className="flex items-center">
+    <>
+      <nav className="z-50 relative bg-zinc-800 text-white px-4 py-4 flex items-center justify-between">
+        <Link to="/" className="flex items-center">
           <img
             className="h-8 rounded me-4"
             src="https://www.pngitem.com/pimgs/m/114-1145783_transparent-book-vector-png-book-icon-vector-png.png"
             alt="logo"
           />
           <h1 className="text-2xl font-semibold">Book Zone</h1>
-        </div>
-        <div className="nav-links-bookzone flex items-center gap-4">
-          <div className="flex gap-4">
+        </Link>
+        <div className="nav-links-bookzone block md:flex items-center gap-4">
+          <div className="hidden md:flex gap-4">
             {links.map((items, i) => (
               <Link
                 to={items.link}
@@ -44,7 +50,7 @@ const Navbar = () => {
               </Link>
             ))}
           </div>
-          <div className="flex gap-4">
+          <div className="hidden md:flex gap-4">
             <Link
               to="/Login"
               className="px-2 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300"
@@ -58,9 +64,45 @@ const Navbar = () => {
               Signup
             </Link>
           </div>
+          <button
+            className="md:hidden text-white text-2xl hover:text-zinc-300"
+            onClick={HandleMobileNav}
+          >
+            <GiHamburgerMenu />
+          </button>
         </div>
+      </nav>
+      {/* //////////////// Mobile Hamburger Details */}
+
+      <div
+        className={` ${MobileNav} bg-zinc-800 h-screen absolute top-0 left-0 w-full z-40 flex flex-col items-center justify-center`}
+      >
+        {links.map((items, i) => (
+          <Link
+            to={items.link}
+            onClick={HandleMobileNav}
+            className={` ${MobileNav} text-white text-4xl mb-8 font-semibold hover:text-blue-500 transition-all duration-300 `}
+            key={i}
+          >
+            {items.title}
+          </Link>
+        ))}
+        <Link
+          to="/Login"
+          onClick={HandleMobileNav}
+          className={`${MobileNav} px-8 py-4 border text-4xl text-white mb-8 font-semibold border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300`}
+        >
+          Login
+        </Link>
+        <Link
+          to="/signup"
+          onClick={HandleMobileNav}
+          className={` ${MobileNav} px-8 py-4 text-4xl mb-8 text-white font-semibold bg-blue-500 rounded  hover:bg-white hover:text-zinc-800 transition-all duration-300`}
+        >
+          Signup
+        </Link>
       </div>
-    </div>
+    </>
   );
 };
 
